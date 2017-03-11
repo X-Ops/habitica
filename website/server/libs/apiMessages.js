adf20d4b4ddee24d71ce149a080c719966a2fda0
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2017-03-11 12:57:48 +0100

    add apiMessages lib with tests

--- website/server/libs/apiMessages.js
+++ website/server/libs/apiMessages.js
@@ -1,28 +1,21 @@
 // A map of messages used by the API that don't need to be translated and
 // so are not placed into /common/locales
 
-import clone from 'lodash/clone';
-import template from 'lodash/template';
-import isFunction from 'lodash/isFunction';
+import _ from 'lodash';
 
 // When this file grows, it can be split into multiple ones.
 const messages = {
-  'groups.paginate.only': 'Only public guilds support pagination.',
+  guildsOnlyPaginate: 'Only public guilds support pagination.',
   guildsPaginateBooleanString: 'req.query.paginate must be a boolean string.',
   guildsPageInteger: 'req.query.page must be an integer greater than or equal to 0.',
 };
 
-export function t (msgKey, vars = {}) {
-  let clonedVars = vars ? clone(vars) : {};
-
+export default function (msgKey, vars = {}) {
   let message = messages[msgKey];
   if (!message) throw new Error(`Error processing the API message "${msgKey}".`);
 
-  if (isFunction(message)) { // message is already compiled and cached
-    return message(clonedVars);
-  } else {
-    let compiled = template(message);
-    messages[msgKey] = compiled; // cache the result of _.template
-    return compiled(clonedVars);
-  }
+  let clonedVars = vars ? _.clone(vars) : {};
+
+  // TODO cache the result of template() ? More memory usage, faster output
+  return _.template(message)(clonedVars);
 }
